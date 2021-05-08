1abdd1d9b4c8ca3100da68951a4f02631905ef62
Agni <1805457@kiit.ac.in>
2021-05-08 17:14:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <React.Fragment>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more React app.</p>
+  </React.Fragment>
+);
+
+export default App;
